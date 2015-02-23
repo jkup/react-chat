27d86f590c800f64365b40d92b7371b7614a4ffd
Jon Kuperman <jon.kuperman@gmail.com>
2015-02-22 18:53:51 -0800

    initial MVP

---
+++ app.js
@@ -0,0 +1,34 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+
+// This should refer to the local React and gets installed via `npm install` in
+// the example.
+var reactViews = require('express-react-views');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', reactViews.createEngine({
+  jsx: {
+    harmony: true,
+    stripTypes: true
+  }
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
